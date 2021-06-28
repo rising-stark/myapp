f0c618c2ffb9e8b5db977e6ab6955b3cb9c32211
rising-stark <harshitkk02@gmail.com>
2021-06-29 00:35:53 +0530

    Updated routes for respective controllers

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookExportController;
 
 Route::get('/', [BookController::class, 'index']);
 
 Route::post('/create-book', [BookController::class, 'createBook'])->name('book.create');
 Route::post('/ajax-delete-book', [BookController::class, 'deleteBook'])->name('ajax.book.delete');
-Route::post('/ajax-update-author', [BookController::class, 'updateAuthor'])->name('ajax.author.update');
+Route::post('/ajax-update-author', [AuthorController::class, 'updateAuthor'])->name('ajax.author.update');
 
-Route::post('/ajax-export-table-csv', [BookController::class, 'exportTableCSV'])->name('ajax.table.export.csv');
-Route::post('/ajax-export-table-xml', [BookController::class, 'exportTableXML'])->name('ajax.table.export.xml');
-Route::post('/ajax-export-table-cur', [BookController::class, 'exportTableCUR'])->name('ajax.table.export.cur');
+Route::post('/ajax-export-table-csv', [BookExportController::class, 'exportTableCSV'])->name('ajax.table.export.csv');
+Route::post('/ajax-export-table-xml', [BookExportController::class, 'exportTableXML'])->name('ajax.table.export.xml');
+Route::post('/ajax-export-table-cur', [BookExportController::class, 'exportTableCUR'])->name('ajax.table.export.cur');
